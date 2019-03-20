8144bf125ebc544c0ed98eddf3171d5d66908575
Geordie Connell <geordie.connell@gmail.com>
2019-03-20 11:07:14 -0400

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import TokenService from '../../services/token-service'
+import IdleService from '../../services/idle-service'
+import './Header.css'
+
+export default class Header extends Component {
+  handleLogoutClick = () => {
+    TokenService.clearAuthToken();
+    TokenService.clearCallbackBeforeExpiry()
+    IdleService.unRegisterIdleResets()
+  }
+
+  renderLogoutLink() {
+    return (
+      <div className='Header__logged-in'>
+        <Link
+          onClick={this.handleLogoutClick}
+          to='/'>
+          Logout
+        </Link>
+      </div>
+    )
+  }
+
+  renderLoginLink() {
+    return (
+      <div className='Header__not-logged-in'>
+        <Link
+          to='/login'>
+          Log in
+        </Link>
+        <Link
+          to='/register'>
+          Register
+        </Link>
+      </div>
+    )
+  }
+
+  render() {
+    return <>
+      <nav className='Header'>
+        <h1>
+          <Link to='/'>
+            City Showdown
+          </Link>
+        </h1>
+        <span className='Header__tagline--wide'>The Ultimate Game of 4 on 4.</span>
+        {TokenService.hasAuthToken()
+          ? this.renderLogoutLink()
+          : this.renderLoginLink()}
+      </nav>
+
+      <span className='Header__tagline--narrow'>The Ultimate Game of 4 on 4.</span>
+    </>
+  }
+}
---
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import { Button, Input, Required } from '../Utils/Utils'
+import AuthApiService from '../../services/auth-api-service'
+
+export default class RegistrationForm extends Component {
+  static defaultProps = {
+    onRegistrationSuccess: () => {}
+  }
+
+  state = { error: null }
+
+  handleSubmit = ev => {
+    ev.preventDefault()
+    const { user_name, password, favorite_city } = ev.target
+
+    this.setState({error: null})
+    AuthApiService.postUser({
+      user_name: user_name.value,
+      password: password.value,
+      favorite_city: favorite_city.value
+    })
+      .then(user => {
+        user_name.value = ''
+        password.value = ''
+        favorite_city.vaue = ''
+        this.props.onRegistrationSuccess()
+      })
+      .catch(res => {
+        this.setState({ error: res.error })
+      })
+  
+  }
+
+  render() {
+    const { error } = this.state
+    return (
+      <form
+        className='RegistrationForm'
+        onSubmit={this.handleSubmit}
+      >
+        <div role='alert'>
+          {error && <p className='red'>{error}</p>}
+        </div>
+        <div className='user_name'>
+          <label htmlFor='RegistrationForm__user_name'>
+            User name <Required />
+          </label>
+          <Input
+            name='user_name'
+            type='text'
+            required
+            id='RegistrationForm__user_name'>
+          </Input>
+        </div>
+        <div className='password'>
+          <label htmlFor='RegistrationForm__password'>
+            Password <Required />
+          </label>
+          <Input
+            name='password'
+            type='password'
+            required
+            id='RegistrationForm__password'>
+          </Input>
+        </div>
+        <div className='favorite_city'>
+          <label htmlFor='RegistrationForm__favorite_city'>
+            Select Your City
+          </label>
+          <Input
+            name='favorite_city'
+            type='text'
+            required
+            id='RegistrationForm__favorite_city'>
+          </Input>
+        </div>
+        <Button type='submit'>
+          Register
+        </Button>
+      </form>
+    )
+  }
+}
